281c15637f4cdaf82a132b67599e15e10fd3b3b5
nart4hire <13520129@std.stei.itb.ac.id>
2024-03-18 01:11:30 +0700

    Updated configuration
    Added:
    - Live reloading of dev server
    - Our gopher logo :))))
    
    TODO:
    - Port frontend to vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import gopherLogo from '/logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={gopherLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>GoBlockC</h1>
+      <p className="read-the-docs">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur corporis quos delectus, quam magni, doloremque in sunt eum cumque inventore sapiente obcaecati, hic nostrum similique. Porro aliquid sunt voluptatem?
+      </p>
+    </>
+  )
+}
+
+export default App
